064a66bd3d08471081e93b39538f652fcc929b1c
Venkatgb <venkatgb92@gmail.com>
2024-12-16 18:43:31 -0700

    Added even and odd check files

---
+++ sample/src/main/java/org/demo/EvenCheck.java
@@ -0,0 +1,13 @@
+Public class EvenCheck
+{
+    public static void main(String[] args) {
+        System.out.println("This code generates the even check for a number");
+
+        int number=10;
+        if(number % 2  == 0)
+        {
+            System.out.println("The provided number is Even");
+        }
+
+    }
+}
